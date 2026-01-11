2a2f6e56b44f6bf48af83ba35c602e94ccc21951
Agormbia <wmdeku@st.ug.edu.gh>
2026-01-11 23:48:57 +0000

    Fix Vercel routing configuration

--- api/[...path].js
+++ api/[...path].js
@@ -1,4 +1,10 @@
 // Vercel serverless function for all API routes
 const app = require('../server/server.js');
 
-module.exports = app;
+module.exports = (req, res) => {
+  // Remove /api prefix since Express routes don't have it
+  if (req.url.startsWith('/api')) {
+    req.url = req.url.replace('/api', '') || '/';
+  }
+  return app(req, res);
+};
