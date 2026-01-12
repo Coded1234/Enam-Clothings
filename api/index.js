804416857a6d04dc41a45863e9170ec28240ebda
Agormbia <wmdeku@st.ug.edu.gh>
2026-01-12 00:06:01 +0000

    Fix API route handling - don't strip /api prefix

--- api/index.js
+++ api/index.js
@@ -2,7 +2,5 @@
 const app = require('../server/server.js');
 
 module.exports = (req, res) => {
-  // Strip /api prefix for Express routes
-  req.url = req.url.replace(/^\/api/, '') || '/';
   return app(req, res);
 };
